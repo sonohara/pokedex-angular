f209eafb2b346e7cda0c6fd12bc73258be210626
sonohara <5350767+sonohara@users.noreply.github.com>
2023-04-25 22:43:43 +0900

    add shuffle

--- src/app/pages/pokemons/pokemon-list/pokemon-list.component.ts
+++ src/app/pages/pokemons/pokemon-list/pokemon-list.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { BehaviorSubject, forkJoin } from 'rxjs';
+import { BehaviorSubject, combineLatest, forkJoin, map } from 'rxjs';
 import { PokemonService, Pokemon, NamedAPIResource } from '@sonohara/pokeapi-typescript-angular';
 import { BgmComponent } from 'src/app/shared/bgm/bgm.component';
 import { MatSelectChange } from '@angular/material/select';
 
-type State = {
+type PokemonsState = {
   pokemons: Pokemon[];
-  pagination: {
-    count: number;
-    page: number;
-  };
 };
 
-const initialState: State = {
+const PokemonsStateInitial: PokemonsState = {
   pokemons: [],
-  pagination: {
-    count: 0,
-    page: 1,
-  },
+};
+
+type PokemonListResource = NamedAPIResource & { id: number };
+
+const sorters: Record<number, (a: PokemonListResource, b: PokemonListResource) => number> = {
+  0: (a: PokemonListResource, b: PokemonListResource) => a.id - b.id,
+  1: (a: PokemonListResource, b: PokemonListResource) => b.id - a.id,
+  2: (a: PokemonListResource, b: PokemonListResource) => (a.name ?? '').localeCompare(b.name ?? ''),
+  3: (a: PokemonListResource, b: PokemonListResource) => (b.name ?? '').localeCompare(a.name ?? ''),
+};
+
+type PaginatorState = {
+  page: number;
+  perpage: number;
+  sorter: (a: PokemonListResource, b: PokemonListResource) => number;
+  filter: (p: PokemonListResource) => boolean;
+};
+
+const PaginatorStateInitial = {
+  page: 1,
+  perpage: 20,
+  sorter: sorters[0],
+  filter: () => true,
+};
+
+type SearchForm = {
+  idOrName: string;
 };
 
 @Component({
@@ -28,8 +47,11 @@ const initialState: State = {
   styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent implements OnInit {
-  readonly state$ = new BehaviorSubject<State>(initialState);
-  searchForm = this.fb.group({
+  readonly pokemons$ = new BehaviorSubject<PokemonsState>(PokemonsStateInitial);
+  readonly paginator$ = new BehaviorSubject<PaginatorState>(PaginatorStateInitial);
+  readonly state$ = combineLatest({ paginator: this.paginator$, pokemons: this.pokemons$ });
+  pokemonResources: PokemonListResource[] = [];
+  searchForm = this.fb.group<SearchForm>({
     idOrName: '',
   });
   sorts = [
@@ -52,57 +74,82 @@ export class PokemonListComponent implements OnInit {
   ];
   selectedSort = this.sorts[0].value;
 
-  constructor(private pokemonService: PokemonService, private fb: FormBuilder, private snackBar: MatSnackBar) {}
-
-  ngOnInit(): void {
-    this.fetchPokemons(this.state$.value.pagination.page);
-    this.snackBar.openFromComponent(BgmComponent);
-  }
-
-  fetchPokemons(page: number, limit = 20, sorter = sorters[0]) {
-    this.pokemonService.getPokemons(9999, 0).subscribe((response) => {
+  constructor(private pokemonService: PokemonService, private fb: FormBuilder, private snackBar: MatSnackBar) {
+    this.paginator$.subscribe((paginator) => {
       forkJoin([
-        ...response.results
-          .map((resource, i) => {
-            return { id: i, ...resource };
-          })
-          .sort(sorter)
-          .slice(limit * (page - 1), limit * page)
+        ...this.pokemonResources
+          .sort(paginator.sorter)
+          .filter(paginator.filter)
+          .slice(paginator.perpage * (paginator.page - 1), paginator.perpage * paginator.page)
           .map((resource) => {
             return this.pokemonService.getPokemonById(resource.name ?? '');
           }),
       ]).subscribe((pokemons) => {
-        this.state$.next({
-          ...this.state$.value,
-          pagination: {
-            count: response.count,
-            page: page,
-          },
-          pokemons: [...this.state$.value.pokemons, ...pokemons],
+        this.pokemons$.next({
+          pokemons: [...this.pokemons$.value.pokemons, ...pokemons],
         });
       });
     });
   }
 
+  ngOnInit(): void {
+    this.fetchPokemons();
+    this.snackBar.openFromComponent(BgmComponent);
+  }
+
+  paginate(page: number) {
+    this.paginator$.next({
+      ...this.paginator$.value,
+      page: page,
+    });
+  }
+
+  fetchPokemons() {
+    this.pokemonService
+      .getPokemons(9999, 0)
+      .pipe(
+        map((response) => {
+          return [
+            ...response.results.map((resource, index) => {
+              return { id: index + 1, ...resource };
+            }),
+          ];
+        })
+      )
+      .subscribe((resources) => {
+        this.pokemonResources = resources;
+        this.paginate(1);
+      });
+  }
+
   search() {
-    console.log(this.searchForm.value);
+    this.pokemons$.next({ pokemons: [] });
+    this.paginator$.next({
+      ...this.paginator$.value,
+      page: 1,
+      filter: this.searchForm.value.idOrName
+        ? (p) =>
+            p.name.includes(this.searchForm.value.idOrName ?? '') ||
+            p.id.toLocaleString().includes(this.searchForm.value.idOrName ?? '0')
+        : this.paginator$.value.filter,
+    });
+  }
+
+  shuffle() {
+    this.pokemons$.next({ pokemons: [] });
+    this.paginator$.next({
+      ...this.paginator$.value,
+      page: 1,
+      sorter: () => Math.random() - 0.5,
+    });
   }
 
   sort(change: MatSelectChange) {
-    console.log(this.selectedSort);
-    this.state$.next({
-      ...this.state$.value,
-      pokemons: [],
+    this.pokemons$.next({ pokemons: [] });
+    this.paginator$.next({
+      ...this.paginator$.value,
+      page: 1,
+      sorter: sorters[change.value],
     });
-    this.fetchPokemons(1, 20, sorters[change.value]);
   }
 }
-
-type PokemonListResource = NamedAPIResource & { id: number };
-
-const sorters: Record<number, (a: PokemonListResource, b: PokemonListResource) => number> = {
-  0: (a: PokemonListResource, b: PokemonListResource) => a.id - b.id,
-  1: (a: PokemonListResource, b: PokemonListResource) => b.id - a.id,
-  2: (a: PokemonListResource, b: PokemonListResource) => (a.name ?? '').localeCompare(b.name ?? ''),
-  3: (a: PokemonListResource, b: PokemonListResource) => (b.name ?? '').localeCompare(a.name ?? ''),
-};
