af534181829a4f216927d68f2aa901b064b9dfb7
sonohara <5350767+sonohara@users.noreply.github.com>
2023-04-27 14:56:20 +0900

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [PagesRoutingModule, RouterModule.forRoot(routes)],
+  imports: [PagesRoutingModule, RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
